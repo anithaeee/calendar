cf751986373da78fe4f9cc1aedc3c1a30ed8f473
anithaeee <anithaeee224@gmail.com>
2023-12-23 16:04:52 +0800

    dec 23rd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Calendar } from 'primereact/calendar';
+import React, {useState} from 'react';
 
 function App() {
+  
+  const[data,setdata]= useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <label>Enter your date
+            <Calendar showIcon value={data} onChange={(e)=>{setdata(e.target.value)}}></Calendar>
+        </label>
+      </form>
     </div>
+    
   );
 }
 
